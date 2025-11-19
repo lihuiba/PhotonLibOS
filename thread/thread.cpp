d1c53b8728b10a2c4b2955c1794b5ed46828f845
lihuiba <huiba.lhb@alibaba-inc.com>
2025-11-19 09:56:47 +0800

    add error log when stack allocation failed

--- thread/thread.cpp
+++ thread/thread.cpp
@@ -991,7 +991,7 @@ R"(
         }
         char* ptr = (char*)photon_thread_alloc(stack_size);
         if (unlikely(!ptr))
-            return nullptr;
+            LOG_ERROR_RETURN(ENOMEM, nullptr, "failed to allocate stack of ` bytes", stack_size)
         uint64_t p = (uint64_t)ptr + stack_size - sizeof(thread) - randomizer;
         p = align_down(p, 64);
         auto th = new ((char*)p) thread;
